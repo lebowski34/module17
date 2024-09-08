5606af3b5e1d0e1d34f3af264b9867c3d37dbdd2
lebowski34 <167763740+lebowski34@users.noreply.github.com>
2024-09-08 17:55:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
+from routers import user, task
 
-from app.routers import task_router, user_router
 
 app = FastAPI()
 
-app.include_router(task_router)
-app.include_router(user_router)
 
 @app.get("/")
-async def root():
+async def welcome():
     return {"message": "Welcome to Taskmanager"}
+
+app.include_router(task.router)
+app.include_router(user.router)
